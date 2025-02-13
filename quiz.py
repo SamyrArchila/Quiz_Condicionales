9e1a2d9b1867e682202b70551bf78999b5f6b37a
SamyrArchila <samirarchila@colegioguanenta.edu.co>
2025-02-13 15:15:14 -0500

    a

--- quiz.py
+++ quiz.py
@@ -8,7 +8,7 @@ print("------------------------------")
 # input
 a = int(input("Ingrese el valor correspondiente al primer lado del triangulo"))
 b = int(input("Ingrese el valor correspondiente al segundo lado del triangulo"))
-c = int(input("Ingrese el valor correspondiente al tercer lado del triangulo")
+c = int(input("Ingrese el valor correspondiente al tercer lado del triangulo"))
 
 
 # processing
